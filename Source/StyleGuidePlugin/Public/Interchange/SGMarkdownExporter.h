6a5237fe23d438f474b97930bc56f732524eaa3b
unknown <grzegorz.muszynski@disroot.org>
2026-02-10 21:40:07 +0100

    Added snubs of exporter code

---
+++ Source/StyleGuidePlugin/Public/Interchange/SGMarkdownExporter.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Exporters/Exporter.h"
+#include "SGMarkdownExporter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STYLEGUIDEPLUGIN_API USGMarkdownExporter : public UExporter
+{
+	GENERATED_UCLASS_BODY()
+
+public:
+	virtual bool ExportText(const FExportObjectInnerContext* Context, UObject* Object, const TCHAR* Type,
+		FOutputDevice& Ar, FFeedbackContext* Warn, uint32 PortFlags = 0) override;
+};
