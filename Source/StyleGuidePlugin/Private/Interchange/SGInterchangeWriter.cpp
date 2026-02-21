1c533be8afa6aa9c609f82dce36e00d82b5a2a21
Grzegorz Muszyński <grzegorz.muszynski@disroot.org>
2026-02-21 22:01:49 +0100

    Delete Source/StyleGuidePlugin directory

--- Source/StyleGuidePlugin/Private/Interchange/SGInterchangeWriter.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Interchange/SGInterchangeWriter.h"
-
-bool USGInterchangeWriter::Export(UInterchangeBaseNodeContainer* BaseNodeContainer) const
-{
-	return Super::Export(BaseNodeContainer);
-}
